6aed7fdae269019275d9550eadb8b7c7a13e8a1a
finnik <niklas@finnik.de>
2023-11-18 23:30:54 +0100

    Before refactor
    Co-authored-by: R. John <RaGit25@users.noreply.github.com>

---
+++ lib/fountains.dart
@@ -0,0 +1,88 @@
+import 'dart:convert';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'package:latlong2/latlong.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class Fountain {
+  LatLng latLng;
+  Widget icon;
+  String name;
+  Fountain(this.latLng, this.icon, this.name);
+}
+
+Future<List<Fountain>> fetchFountains() async {
+  var response = await http.get(Uri.parse(
+      'https://geoportal.muenchen.de/geoserver/baug_wfs/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=baug_wfs:opendata_trinkwasserbrunnen&outputFormat=application/json&srsName=EPSG:4326'));
+  var json = jsonDecode(response.body) as Map<String, dynamic>;
+  List<Fountain> list = List.empty(growable: true);
+  for (var element in json['features']) {
+    var coord = element['geometry']['coordinates'];
+    var latLng = LatLng(coord[1], coord[0]);
+    Fountain fountain = Fountain(
+        latLng,
+        IconButton(
+          onPressed: () {
+            navigateToLatLng(latLng);
+          },
+          icon: const Icon(
+            Icons.water_drop,
+            size: 20,
+            color: Colors.blue,
+          ),
+        ),
+        element['properties']['bezeichnung']);
+    list.add(fountain);
+  }
+  response = await http.get(Uri.parse(
+      'https://api.ofdb.io/v0/search?bbox=48.05949581959207%2C11.346473693847658%2C48.192412189186236%2C11.655464172363281&text=refill-station%20refill-trinkbrunnen%20refill%20refill-sticker%20trinkwasser%20leitungswasser%20trinkwasserbrunnen%20trinkbrunnen&categories=2cd00bebec0c48ba9db761da48678134%2C77b3c33a92554bcf8e8c2c86cedd6f6f'));
+  json = jsonDecode(response.body) as Map<String, dynamic>;
+  for (var element in json['visible']) {
+    var latLng = LatLng(element['lat'], element['lng']);
+    Fountain m = Fountain(
+      latLng,
+      IconButton(
+        onPressed: () {
+          navigateToLatLng(latLng);
+        },
+        icon: const Icon(
+          Icons.water_drop,
+          size: 20,
+          color: Colors.blue,
+        ),
+      ),
+      element['title'],
+    );
+    list.add(m);
+  }
+  return list;
+}
+
+Fountain findNearestFountain(LatLng user, List<Fountain> fountains) {
+  fountains.sort((f1, f2) =>
+      distance(user, f1.latLng).compareTo(distance(user, f2.latLng)));
+  return fountains.first;
+  // TODO
+}
+
+double distance(LatLng one, LatLng two) {
+  return Geolocator.distanceBetween(
+      one.latitude, one.longitude, two.latitude, two.longitude);
+}
+
+void navigateToFountain(Fountain fountain) async {
+  navigateToLatLng(fountain.latLng);
+}
+
+void navigateToLatLng(LatLng latLng) async {
+  String googleUrl =
+      'https://www.google.com/maps/search/?api=1&query=${latLng.latitude},${latLng.longitude}';
+
+  if (await canLaunchUrl(Uri.parse(googleUrl))) {
+    await launchUrl(Uri.parse(googleUrl));
+  } else {
+    throw "Couldn't launch Map";
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hackatum23/fountains.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +18,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: Colors.blue,
+          scaffoldBackgroundColor: const Color.fromARGB(255, 212, 245, 254),
+          fontFamily: GoogleFonts.raleway().fontFamily),
       home: const MyHomePage(title: 'Wasser für München'),
     );
   }
@@ -31,67 +38,129 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  MapController mapController = MapController(
-    initPosition: GeoPoint(latitude: 48.137648, longitude: 11.574628),
-    areaLimit: const BoundingBox.world(),
-  );
-
-  void _addMarkers() {
-    fetchFountains().then((value) {
-      for (var element in value) {
-        mapController.addMarker(element);
-      }
-    });
+  List<Fountain>? _fountains;
+  LatLng? _user;
+  Widget buildMap() {
+    MapController controller = MapController();
+    return FlutterMap(
+      mapController: controller,
+      options: const MapOptions(
+        initialCenter: LatLng(48.137648, 11.574628),
+        initialZoom: 13,
+        minZoom: 10,
+        maxZoom: 19,
+      ),
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'com.example.app',
+        ),
+        _fountains == null
+            ? const CircularProgressIndicator()
+            : MarkerLayer(
+                markers: _fountains!
+                    .map((e) => Marker(point: e.latLng, child: e.icon))
+                    .toList(),
+              ),
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    fetchFountains().then((list) {
+      setState(() {
+        _fountains = list;
+      });
+    });
+    Geolocator.getCurrentPosition().then((position) {
+      setState(() {
+        _user = LatLng(position.latitude, position.longitude);
+      });
+    });
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Stack(
-        children: <Widget>[
-          OSMFlutter(
-            controller: mapController,
-            osmOption: OSMOption(
-              zoomOption: const ZoomOption(
-                initZoom: 11,
-                minZoomLevel: 10,
-                maxZoomLevel: 19,
-                stepZoom: 1.0,
-              ),
-              userLocationMarker: UserLocationMaker(
-                personMarker: const MarkerIcon(
-                  icon: Icon(
-                    Icons.location_history_rounded,
-                    color: Colors.red,
-                    size: 48,
-                  ),
-                ),
-                directionArrowMarker: const MarkerIcon(
-                  icon: Icon(
-                    Icons.double_arrow,
-                    size: 48,
-                  ),
-                ),
-              ),
-              roadConfiguration: RoadOption(
-                roadColor: Colors.yellowAccent,
-              ),
-              markerOption: MarkerOption(
-                  defaultMarker: const MarkerIcon(
-                icon: Icon(
-                  Icons.water_drop,
-                  color: Colors.blue,
-                  size: 30,
-                ),
-              )),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
             ),
-          )
-        ],
+            const Center(
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Image(
+                  image: AssetImage('assets/logo.png'),
+                  height: 75,
+                ),
+              ),
+            ),
+            Stack(
+              children: [
+                SizedBox(
+                  height: 400,
+                  child: buildMap(),
+                ),
+                Center(
+                  child: Card(
+                    clipBehavior: Clip.hardEdge,
+                    child: InkWell(
+                      splashColor: Colors.blue.withAlpha(30),
+                      onTap: () async {
+                        //TODO open navigation
+                        if (_user == null || _fountains == null) {
+                          return;
+                        }
+                        navigateToFountain(
+                            findNearestFountain(_user!, _fountains!));
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            const Icon(Icons.water_drop_outlined),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 15, right: 15),
+                              child: _fountains == null || _user == null
+                                  ? const CircularProgressIndicator()
+                                  : Column(
+                                      children: [
+                                        Text(findNearestFountain(
+                                                _user!, _fountains!)
+                                            .name),
+                                        Text(
+                                            "${distance(_user!, findNearestFountain(_user!, _fountains!).latLng).round()}m")
+                                      ],
+                                    ),
+                            ),
+                            const Column(
+                              children: [
+                                Icon(Icons.turn_right),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: _addMarkers),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        const BottomNavigationBarItem(
+          label: "Home",
+          icon: Icon(Icons.home),
+        ),
+        const BottomNavigationBarItem(
+          label: "Info",
+          icon: Icon(Icons.info),
+        ),
+      ]),
     );
   }
 }
